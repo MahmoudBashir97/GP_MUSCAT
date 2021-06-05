ba0b82bd7b57665e4df7aba0a4c652df48a181c8
Mahmoud Bashir <anadeouf@yahoo.com>
2021-06-05 16:29:02 +0200

    Mahmoud Committed

--- app/src/main/java/com/example/loginpage/StudentDashboard.java
+++ app/src/main/java/com/example/loginpage/StudentDashboard.java
@@ -1,15 +1,65 @@
 package com.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class StudentDashboard extends AppCompatActivity {
+import com.example.loginpage.LocalStorage.SharedPrefranceManager;
+import com.example.loginpage.ui.MainActivity;
 
+public class StudentDashboard extends AppCompatActivity {
+    private CardView student_to_chat_btn,student_to_files_btn,student_to_attendance_btn,student_to_quiz_btn,logout_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_dashboard);
+        student_to_chat_btn = findViewById(R.id.student_to_chat_btn);
+        student_to_files_btn = findViewById(R.id.student_to_files_btn);
+        student_to_attendance_btn = findViewById(R.id.student_to_attendance_btn);
+        student_to_quiz_btn = findViewById(R.id.student_to_quiz_btn);
+        logout_btn = findViewById(R.id.logout_btn);
+
+        navTo_AttendanceActivity();
+        navToChatActivity();
+        navToFilesActivity();
+        navToQuizActivity();
+
+        doLogout();
+
+    }
+
+    private void doLogout(){
+        logout_btn.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class)
+            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+            finish();
+            SharedPrefranceManager.getInastance(getApplicationContext()).clearUser();
+        });
+    }
+
+    private void navToChatActivity(){
+        student_to_chat_btn.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(),Chat.class));
+        });
+    }
+
+    private void navToFilesActivity(){
+        student_to_files_btn.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(),Files.class));
+        });
+    }
+
+    private void navTo_AttendanceActivity(){
+            student_to_attendance_btn.setOnClickListener(v -> {
+                startActivity(new Intent(getApplicationContext(),Attendance.class));
+            });
+    }
+    private void navToQuizActivity(){
+                student_to_quiz_btn.setOnClickListener(v -> {
+                    startActivity(new Intent(getApplicationContext(),DownloadQuiz.class));
+                });
     }
 
 }
--- app/src/main/java/com/example/loginpage/TeacherDashBoard.java
+++ app/src/main/java/com/example/loginpage/TeacherDashBoard.java
@@ -1,14 +1,65 @@
 package com.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.loginpage.LocalStorage.SharedPrefranceManager;
+import com.example.loginpage.ui.MainActivity;
+
 public class TeacherDashBoard extends AppCompatActivity {
+
+    private CardView teacher_to_chat_btn,teacher_to_files_btn,teacher_to_attendance_btn,teacher_to_quiz_btn,logout_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_dash_borad);
+        teacher_to_chat_btn = findViewById(R.id.teacher_to_chat_btn);
+        teacher_to_files_btn = findViewById(R.id.teacher_to_files_btn);
+        teacher_to_attendance_btn = findViewById(R.id.teacher_to_attendance_btn);
+        teacher_to_quiz_btn = findViewById(R.id.teacher_to_quiz_btn);
+        logout_btn = findViewById(R.id.logout_btn);
+
+        navTo_AttendanceActivity();
+        navToChatActivity();
+        navToFilesActivity();
+        navToQuizActivity();
+
+        doLogout();
+
+    }
+    private void doLogout(){
+        logout_btn.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+            finish();
+            SharedPrefranceManager.getInastance(getApplicationContext()).clearUser();
+        });
+    }
+
+    private void navToChatActivity(){
+        teacher_to_chat_btn.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(),Chat.class));
+        });
+    }
+
+    private void navToFilesActivity(){
+        teacher_to_files_btn.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(),Files.class));
+        });
+    }
+
+    private void navTo_AttendanceActivity(){
+        teacher_to_attendance_btn.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(),Attendance.class));
+        });
+    }
+    private void navToQuizActivity(){
+        teacher_to_quiz_btn.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(),DownloadQuiz.class));
+        });
     }
 }
